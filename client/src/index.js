8ba59f2f6408bd8d6cdbc77b6bec59ab2d812887
eLafo <javierlafora@gmail.com>
2018-05-18 14:33:43 +0200

    addds js and html entrypoints

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+// import your main App Component here
+// import YourComponent from 'components/YourComponent'
+
+const wrapper = document.getElementById('app');
+// Uncomment the following lines to mount the main component
+// wrapper ? ReactDOM.render(<YourComponent />, wrapper) : false
