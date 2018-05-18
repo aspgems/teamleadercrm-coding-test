98d691f43e3be16a62985a22a249bf4d94ee31eb
eLafo <javierlafora@gmail.com>
2018-05-18 14:25:15 +0200

    addds js and html entrypoints

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,7 @@
 
 import ReactDOM from 'react-dom';
 import React from 'react';
-// import your main App Component here
-// import YourComponent from 'components/YourComponent'
+import FormContainer from "./components/functional/FormContainer";
 
 const wrapper = document.getElementById('app');
-// Uncomment the following lines to mount the main component
-// wrapper ? ReactDOM.render(<YourComponent />, wrapper) : false
+wrapper ? ReactDOM.render(<FormContainer />, wrapper) : false
